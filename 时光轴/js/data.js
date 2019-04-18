1587bdc3f8dc31a5a95b09a19d6d2d2befd758de
wangyunxizhe <543456229@qq.com>
2019-04-18 17:45:23 +0800

    时光轴效果浏览日志，包含日期转农历插件js

---
+++ 时光轴/js/data.js
@@ -0,0 +1,148 @@
+var data = [];
+
+data.push({
+    'date'  : '2014-3-31',
+    'intro' : '测试日志内容数据1',
+    'media' : '<img src="images/psb.jpeg" width="370" >',
+    'like' : 302,
+    'comment' : 883
+})
+
+
+data.push({
+    'date'  : '2014-3-30',
+    'intro' : '测试日志内容数据2>><a href="#">http://url.cn/MbxmrY</a>',
+    'media' : '<img src="images/psb.jpeg" width="370" >',
+    'like' : 35038,
+    'comment' : 817
+})
+
+data.push({
+    'date'  : '2014-3-30',
+    'intro' : '测试日志内容数据3',
+    'media' : '<img src="images/psb.jpeg" width="370" >',
+    'like' : 25038,
+    'comment' : 417
+})
+
+data.push({
+    'date'  : '2014-3-28',
+    'intro' : '测试日志内容数据4',
+    'media' : '<img src="images/psb.jpeg" width="370" >',
+    'like' : 25038,
+    'comment' : 417
+})
+
+data.push({
+    'date'  : '2014-2-28',
+    'intro' : '测试日志内容数据5',
+    'media' : '<img src="images/psb.jpeg" width="370" >',
+    'like' : 25038,
+    'comment' : 417
+})
+
+data.push({
+    'date'  : '2014-2-27',
+    'intro' : '测试日志内容数据6',
+    'media' : '<img src="images/psb.jpeg" width="370" >',
+    'like' : 25038,
+    'comment' : 417
+})
+
+data.push({
+    'date'  : '2014-2-26',
+    'intro' : '测试日志内容数据7',
+    'media' : '<img src="images/psb.jpeg" width="370" >',
+    'like' : 25038,
+    'comment' : 417
+})
+
+data.push({
+    'date'  : '2014-2-02',
+    'intro' : '测试日志内容数据8',
+    'media' : '<img src="images/psb.jpeg" width="370" >',
+    'like' : 238,
+    'comment' : 417
+})
+data.push({
+    'date'  : '2014-2-01',
+    'intro' : '测试日志内容数据9',
+    'media' : '<img src="images/psb.jpeg" width="370" >',
+    'like' : 25038,
+    'comment' : 417
+})
+
+
+
+
+data.push({
+    'date'  : '2013-3-31',
+    'intro' : '测试日志内容数据10',
+    'media' : '<img src="images/psb.jpeg" width="370" >',
+    'like' : 32102,
+    'comment' : 883
+})
+
+
+data.push({
+    'date'  : '2013-3-30',
+    'intro' : '测试日志内容数据11>><a href="#">http://url.cn/MbxmrY</a>',
+    'media' : '<img src="images/psb.jpeg" width="370" >',
+    'like' : 35038,
+    'comment' : 817
+})
+
+data.push({
+    'date'  : '2013-3-30',
+    'intro' : '测试日志内容数据12',
+    'media' : '<img src="images/psb.jpeg" width="370" >',
+    'like' : 25038,
+    'comment' : 417
+})
+
+data.push({
+    'date'  : '2013-3-28',
+    'intro' : '测试日志内容数据13',
+    'media' : '<img src="images/psb.jpeg" width="370" >',
+    'like' : 25038,
+    'comment' : 417
+})
+
+data.push({
+    'date'  : '2013-2-28',
+    'intro' : '测试日志内容数据14',
+    'media' : '<img src="images/psb.jpeg" width="370">',
+    'like' : 25038,
+    'comment' : 417
+})
+
+data.push({
+    'date'  : '2013-2-27',
+    'intro' : '测试日志内容数据15',
+    'media' : '<img src="images/psb.jpeg" width="370" >',
+    'like' : 25038,
+    'comment' : 417
+})
+
+data.push({
+    'date'  : '2013-2-26',
+    'intro' : '测试日志内容数据16',
+    'media' : '<img src="images/psb.jpeg" width="370" >',
+    'like' : 25038,
+    'comment' : 417
+})
+
+data.push({
+    'date'  : '2013-2-02',
+    'intro' : '测试日志内容数据17',
+    'media' : '<img src="images/psb.jpeg" width="370" >',
+    'like' : 25038,
+    'comment' : 417
+})
+data.push({
+    'date'  : '2013-2-01',
+    'intro' : '测试日志内容数据18',
+    'media' : '<img src="images/psb.jpeg" width="370" >',
+    'like' : 25038,
+    'comment' : 417
+})
